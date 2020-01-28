6e6d5f7bd6486152f3946fa2f3805157cf23180d
vagnerPaulino <vagnerpaulinop@gmail.com>
2020-01-28 19:45:56 -0200

    Obtendo um usuário por id

--- src/main/java/com/paulinoVagnerworkshopmongo/services/UserService.java
+++ src/main/java/com/paulinoVagnerworkshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.paulinoVagnerworkshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.paulinoVagnerworkshopmongo.domain.User;
 import com.paulinoVagnerworkshopmongo.repository.UserRepository;
+import com.paulinoVagnerworkshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,9 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();		
 	}
-
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
